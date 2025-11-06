5e3afb2131db845273a2a55344a2dce2cd3c74f9
Lucas Wojaczek <lwojaczek@bacs-consulting.com>
2025-11-06 13:50:47 +0100

    add mongoDB and test connection ok

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://main_db_user:HTDLW0bjKMeXkinm@cluster-001.dfhq8gi.mongodb.net/?appName=cluster-001',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+module.exports = app;
