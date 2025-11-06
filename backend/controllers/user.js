07c6f8eddc27c9a7e0102b6ca41c4cd1b9f6d541
Lucas Wojaczek <lwojaczek@bacs-consulting.com>
2025-11-06 16:22:27 +0100

    add logic to signup and login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-undef */
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../modeles/User')
 
@@ -21,5 +22,26 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
+   User.findOne({ email: req.body.email })
+       .then(user => {
+           if (!user) {
+               return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+           }
+           bcrypt.compare(req.body.password, user.password)
+               .then(valid => {
+                   if (!valid) {
+                       return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                   }
+                   res.status(200).json({
+                       userId: user._id,
+                       token: jwt.sign(
+                        { userId: user._id },
+                        'RANDOM_TOKEN_SECRET',
+                        { expiresIn: '24h' }
+                    )
+                   });
+               })
+               .catch(error => res.status(500).json({ error }));
+       })
+       .catch(error => res.status(500).json({ error }));
 };
